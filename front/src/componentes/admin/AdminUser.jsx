0e204892a53987cbf4d3f647c43ebbf0e15c5f7e
Facu Sanchez <facusanchz96@gmail.com>
2023-12-30 22:27:38 -0300

    impt a.u

--- front/src/componentes/admin/AdminUser.jsx
+++ front/src/componentes/admin/AdminUser.jsx
@@ -7,7 +7,7 @@ import { jwtDecode } from "jwt-decode";
 import { useNavigate } from 'react-router-dom';
 import Form from '../form/Form'
 import PosibleUsers from '../posibleUsers/PosibleUsers'
-import AllUsers from '../users/AllUsers'
+import AllUsers from "../allusers/AllUsers";
 
 
 const AdminUser = () => {
