26ebbed1c2d2a1ef859c90e2e1cc7fa3600f3b13
Facu Sanchez <facusanchz96@gmail.com>
2024-02-12 20:57:55 -0300

    stilo

--- front/src/componentes/nav/Nav.jsx
+++ front/src/componentes/nav/Nav.jsx
@@ -4,14 +4,14 @@ import style from './style.module.css';
 import Searchbar from "../searchBar/SearchBar";
 
 import { useEffect, useState } from "react";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { jwtDecode } from "jwt-decode";
 
 import { Link } from "react-router-dom";
 
 const Nav = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
-
+    const { pathname } = useLocation()
 
     const [nivel, setNivel] = useState('');
     const dispatch = useDispatch();
@@ -25,7 +25,7 @@ const Nav = () => {
                 console.log(token);
                 const decodedToken = jwtDecode(token);
                 setNivel(decodedToken.nivel);
-      
+
                 dispatch(userID(decodedToken.usuarioId));
             } else {
                 throw new Error('El token no está presente en el objeto respuesta');
@@ -49,30 +49,41 @@ const Nav = () => {
 
 
 
-let rol
+    let rol
     const usuario = useSelector(state => state.user);
     return (
 
         <div className={style.navBarContainer}>
-           
-                <div className={style.logoContainer}>
-                    <Link to={'/home'}>
-                  <img src="https://i.pinimg.com/originals/f4/7f/d1/f47fd1fdc887f5cec9f3f2318c3c8313.png" alt="8" title="Home" width={'45rem'} />
-                    </Link>
-                
-                </div>
-<Searchbar></Searchbar>
-       <p>Bienvenido {rol = usuario?.nivel === '2' ? 'Usuario' : usuario?.nivel === '3' ?'Admin': ''} {usuario.nombreCompleto}</p>
+
+            <div className={style.logoContainer}>
+                <Link to={'/home'}>
+                    <img src="https://i.pinimg.com/originals/f4/7f/d1/f47fd1fdc887f5cec9f3f2318c3c8313.png" alt="8" title="Home" width={'45rem'} />
+                </Link>
+
+
+            </div>
+            {
+
+                pathname === '/home' ?
+                    <Searchbar></Searchbar>
+                    : ''
+            }
+            {
+
+                pathname === '/home' ?
+                    <p>Bienvenido {rol = usuario?.nivel === '2' ? 'Usuario' : usuario?.nivel === '3' ? 'Admin' : ''} {usuario.nombreCompleto}</p>
+                    : ''
+            }
             <div className={style.userContainer}>
-     
-       
-     
+
+
+
                 <Link to="#" className={style.customUserIcon} onClick={handleMenuToggle}>               <img
 
-src='https://static.vecteezy.com/system/resources/previews/020/911/737/non_2x/user-profile-icon-profile-avatar-user-icon-male-icon-face-icon-profile-icon-free-png.png'
-alt="co" width='56px'
-className='{style.img}' title=" Mi Perfil"
-/></Link>
+                    src='https://static.vecteezy.com/system/resources/previews/020/911/737/non_2x/user-profile-icon-profile-avatar-user-icon-male-icon-face-icon-profile-icon-free-png.png'
+                    alt="co" width='56px'
+                    className='{style.img}' title=" Mi Perfil"
+                /></Link>
                 {isMenuOpen && (
                     <div className={style.hamburgerMenuContainer}>
                         <div className={style.hamburgerMenu}>
@@ -85,9 +96,9 @@ className='{style.img}' title=" Mi Perfil"
                                 : (
                                     <>
                                         <Link to={`/user/${usuario.id}`} className={style.menuItem}>Perfil</Link>
-                                        { usuario.nivel == "2" && (
+                                        {usuario.nivel == "2" && (
                                             <>
-                                            <Link to="/form" className={style.menuItem}>Agregar pasajero</Link>
+                                                <Link to="/form" className={style.menuItem}>Agregar pasajero</Link>
                                             </>
                                         )}
                                         {usuario.nivel == "3" && (
@@ -105,7 +116,7 @@ className='{style.img}' title=" Mi Perfil"
                     </div>
                 )}
             </div>
-   
+
 
 
 
