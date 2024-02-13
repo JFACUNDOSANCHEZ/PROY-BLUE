8c490af7f8e2eab55a43d9d7114306611bcafa6d
Facu Sanchez <facusanchz96@gmail.com>
2024-02-13 18:56:34 -0300

    Delet

--- front/src/componentes/allusers/AllUsers.jsx
+++ front/src/componentes/allusers/AllUsers.jsx
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
-import {  findUserName, user, allUsers, userPut, deleteUser } from "../../redux/actions";
+import { findUserName, user, allUsers, userPut, deleteUser, allPasseger } from "../../redux/actions";
 import { useEffect, useState } from "react";
 import styles from './style.module.css';
 import PasForUser from "../pasForUser/PasForUser";
@@ -10,11 +10,25 @@ import Footer from '../footer/Footer'
 
 
 const AllUsers = () => {
+
+
   const dispatch = useDispatch();
   const [selectedUsers, setSelectedUsers] = useState({});
+  const [pass, setPass] = useState("");
+  const [filter, setFilter] = useState('Todos');
+  const [pagina, setPagina] = useState(1);
+  const [porPagina, setPorPagina] = useState(10)
+  const [confirmDelete, setConfirmDelete] = useState(false);
+const [edit, setEdit]= useState(null)
+
+
+
   useEffect(() => {
     dispatch(allUsers())
-  }, []); 
+    dispatch(allPasseger())
+  }, []);
+
+
   const handleChange = (id) => (e) => {
     const activo = e.target.value === 'T' ? "true" : "false";
     const data = { activo: activo }
@@ -28,13 +42,20 @@ const AllUsers = () => {
     setSelectedUsers({ nivel: selectedLevel });
     dispatch(userPut({ nivel: selectedLevel }, userId))
   };
-  console.log(selectedUsers);
-  const [pass, setPass] = useState("");
-  const [filter, setFilter] = useState('Todos');
+  let userIdS = []
+  const passegers = useSelector(state => state.allpasseger)
+  passegers.forEach(p => userIdS.push(parseInt(p.userId)))
+
+  // const userIds = passengers.map(passenger => passenger.userId);
+  // console.log('aca tenemos a ' + userIDS );
   const Users = useSelector(state => state.users);
-  
-  const soli = Users.filter((u)=>{return u.activo === "pendiente"})
+  const soli = Users.filter((u) => { return u.activo === "pendiente" })
+
+
+
+
   let users;
+
   if (filter === 'Solicitud') {
     users = Users.filter((u) => u.activo === "pendiente");
   } else if (filter === 'Activo') {
@@ -44,75 +65,78 @@ const AllUsers = () => {
   } else {
     users = Users;
   }
-  
-  
+  const maximo = Math.ceil(users.length / porPagina);
+
+
   users = pass ? users.filter((u) => {
     const nombreMatch = u.nombreCompleto.toLowerCase().includes(pass.toLowerCase());
     const correoMatch = u.correoElectronico.toLowerCase().includes(pass.toLowerCase());
     return nombreMatch || correoMatch;
   }) : users;
-  
-  
+
+
   const handleFind = (event) => {
     const name = event.target.value;
     setPass(name);
   };
-  
-  
+
+
   const handleFilter = (filtro) => {
     setFilter(filtro);
   };
-  const [pagina, setPagina] = useState(1);
-  const [porPagina, setPorPagina] = useState(10)
-  const maximo = Math.ceil(users.length / porPagina);
-  const [confirmDelete, setConfirmDelete] = useState(false);
 
-  const handleDeletUser =(id)=>{
+  const handleDeletUser = (id) => {
     if (confirmDelete) {
       dispatch(deleteUser(id));
       setConfirmDelete(false); // Resetear el estado de confirmación
     }
   }
   
+  const handleEdit=()=>{
+setEdit(!edit)
+
+  }
+console.log(edit);
   return (
     <div className={styles.homeContainer}>
-      {/* <Nav></Nav> */}
-      <div className={styles.navBar}>
-      </div>
-      <div className={styles.contetTitle}>
+      <Nav></Nav>
+                  <div className={styles.navBar}>
+                  </div>
+                  <div className={styles.contetTitle}>
         <h1 className={styles.title}>Admin DashBoard</h1>
       </div>
-          <h2>Tabla de Usuarios</h2>
+      <h2>Tabla de Usuarios</h2>
+
       <div className={styles.contentTable}>
         <div className={styles.tableHeader}>
           <div className={styles.searchBox}>
-  <button className={styles.button} onClick={() => handleFilter('Todos')}>Todos</button>
-  <button className={styles.button} onClick={() => handleFilter('Solicitud')}>Solicitudes<span className={styles.solicitudCount}>{soli.length}</span></button>
-  <button className={styles.button} onClick={() => handleFilter('Activo')}>Activos</button>
-  <button className={styles.button} onClick={() => handleFilter('Inactivo')}>Inactivos</button>
-</div>
-  <input
-    className={styles.inputSearch}
-    onChange={handleFind}
-    type="search"
-    placeholder="Busca por el nombre o mail.."
-  />
-          
-      
+                <button className={styles.button} onClick={() => handleFilter('Todos')}>Todos</button>
+                <button className={styles.button} onClick={() => handleFilter('Solicitud')}>Solicitudes<span className={styles.solicitudCount}>{soli.length}</span></button>
+                <button className={styles.button} onClick={() => handleFilter('Activo')}>Activos</button>
+                <button className={styles.button} onClick={() => handleFilter('Inactivo')}>Inactivos</button>
+          </div>
+          <input
+            className={styles.inputSearch}
+            onChange={handleFind}
+            type="search"
+            placeholder="Busca por el nombre o mail.."
+          />
+
+
         </div>
-            <Paginacion pagina={pagina} setPagina={setPagina} maximo={maximo} />
+        <Paginacion pagina={pagina} setPagina={setPagina} maximo={maximo} />
         <table className={styles.userTable}>
           <thead>
             <tr className={styles.tableHeaderRow}>
-              <th className={styles.users}>Users</th>
+              <th className={styles.users}>Nombre Usuario</th>
               <th className={styles.estado}>Estado</th>
               <th className={styles.rol}>Rol</th>
               <th className={styles.correo}>Email</th>
               <th className={styles.rol}>Fecha de alta</th>
-              <th className={styles.rol}>Cancelar / Activar usuario</th>
-              <th className={styles.rol}>Cambiar rol</th>
+              <th className={styles.rol}></th>
+              <th className={styles.rol}></th>
               <th className={styles.rol}>   {/* Espacio para los botones*/}</th>
-              <th className={styles.rol}>   {/* Espacio para los botones*/}</th>
+        
             </tr>
           </thead>
           <tbody>
@@ -120,62 +144,92 @@ const AllUsers = () => {
               const fecha = new Date(user?.createdAt);
               const fechaFormateada = fecha.toLocaleDateString();
               const horaFormateada = fecha.toLocaleTimeString();
+
+
+
+
               return <tr key={user.id} className={styles.hover}>
+
+
+
                 <td>{user?.nombreCompleto}</td>
                 <td className={
-                  user.activo === "true"? styles.activo : 
-                  user.activo === "false" ? styles.inactivo :
-                  styles.pendiente
+                  user.activo === "true" ? styles.activo :
+                    user.activo === "false" ? styles.inactivo :
+                      styles.pendiente
                 }>
-                   {user?.activo === "true" ? 'Activo' : user?.activo === "false" ? 'Inactivo' : 'Pendiente'}</td>
-                <td >{user?.nivel === '3' ? 'Administrador' : user?.nivel === '2' ? 'Usuario' : 'Usuario'}</td>
-                <td>{user?.correoElectronico}</td>
-                <td>{fechaFormateada} <br /> {horaFormateada}hs</td>
-               
-                <td>        <select onChange={handleChange(user?.id)} >
-//               <option>--</option>
-//               <option value="F">Cancelar usuario</option>
-//               <option value="T">Habilitar cuenta</option>
-//             </select>
+                {
+!edit ? 
+                  user?.activo === "true" ? 'Activo' : user?.activo === "false" ? 'Inactivo' : 'Pendiente'
+                  :
+                  <select onChange={handleChange(user?.id)} className={styles.select} >
+                                <option>--</option>
+                                <option value="F">Cancelar usuario</option>
+                                <option value="T">Activar usuario</option>
+                              </select>
+                  
+                }
                 </td>
-                <td>
-                  <select onChange={(e) => handleLevelChange(e, user?.id)}>
+                <td >
+                {
+!edit ? 
+                  
+                  user?.nivel === '3' ? 'Administrador' : user?.nivel === '2' ? 'Usuario' : 'Usuario'
+                  :
+                  
+                  
+                  <select onChange={(e) => handleLevelChange(e, user?.id)} className={styles.select}>
                     <option  >--</option>
-       
+
                     <option value="2">Usuario</option>
                     <option value="3">Administrador</option>
                   </select>
-               
-                </td>
-                <td>
+                  
+                }
+                  </td>
+                <td>{user?.correoElectronico}</td>
+                <td>{fechaFormateada} <br /> {horaFormateada}hs</td>
 
-                <button
-  className={styles.viewButton}
-  onClick={() => setConfirmDelete(true)} 
->Eliminar pasajero</button>
-
-                </td>
-  {confirmDelete && (
-  <div className={styles.overla}>
-  <div className={styles.overlayContent}>
-  <p>¿Estás seguro de que quieres eliminar este pasajero?</p>
-  <button onClick={() => handleDeletUser(user.id)}>Sí</button>
-  <button onClick={() => setConfirmDelete(false)}>Cancelar</button>
-  </div>
-  </div>
-  )}
+                {
+!edit ? 
+            ''
+                :
                 <td>
-<Link to={`/pasforuser/${user?.id}`}>
-                  <button className={styles.viewButton}   >Ver Publicaciónes</button>
-</Link>
+                  {userIdS.includes(user?.id) ?
+                    ''
+                    :
+
+                    <button
+                    className={styles.viewButton}
+                    onClick={() => setConfirmDelete(true)}
+                    >Eliminar pasajero</button>
+                  }
                 </td>
+            }
+                {confirmDelete && (
+                  <div className={styles.overla}>
+                    <div className={styles.overlayContent}>
+                      <p>¿Estás seguro de que quieres eliminar este pasajero?</p>
+                      <button onClick={() => handleDeletUser(user.id)}>Sí</button>
+                      <button onClick={() => setConfirmDelete(false)}>Cancelar</button>
+                    </div>
+                  </div>
+                )}
+                <td>
+                  <Link to={`/pasforuser/${user?.id}`}>
+                    <button className={styles.viewButton}   >Ver Publicaciónes</button>
+                  </Link>
+                </td>
+                    <td>
+                      <button onClick={handleEdit}  className={styles.viewButton} >{ !edit ? 'editar' : 'Listo' }  </button>
+                    </td>
               </tr>
             })}
           </tbody>
         </table>
-        
+
       </div>
-<Footer></Footer>
+      <Footer></Footer>
     </div>
   );
 };
