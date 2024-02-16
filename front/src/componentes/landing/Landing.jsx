45cfdeff547c552d979effe33563d6e430adfc5d
Facu Sanchez <facusanchz96@gmail.com>
2024-02-16 20:39:51 -0300

    estilol

--- front/src/componentes/landing/Landing.jsx
+++ front/src/componentes/landing/Landing.jsx
@@ -35,7 +35,7 @@ const Landing = () => {
     <div className={styles.landingContainer}>
       <h1 className={styles.ELh1}><strong>BLU.</strong></h1>
       <h2 className={styles.h2}>Seguridad en tus manos</h2>
-      <button onClick={toggleModal} className={styles.buton}>Login</button>
+      <button onClick={toggleModal} className={styles.buton}>Ingresar</button>
       <button onClick={toggleModalRegister} className={styles.buton}>Registro</button>
       {showModal && (
         <div className={styles['modal-overlay']}>
